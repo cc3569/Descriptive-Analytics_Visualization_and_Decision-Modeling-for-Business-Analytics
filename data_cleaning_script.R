c1a2ed3921432ec18f8be5986c4ceeae4c929913
Cole T. Catron, B.S., B.S.B.A. <cc3569@nau.edu>
2024-09-10 11:57:31 -0700

    Add files via upload

---
+++ data_cleaning_script.R
@@ -0,0 +1,20 @@
+#imports mosaic library
+library(mosaic)
+
+#imports cleaned dataset for Consumer Airfare Report - Table 1 (2018 - Q1 2024)
+d = read.csv("https://raw.githubusercontent.com/cc3569/Descriptive-Analytics_Visualization_and_Decision-Modeling-for-Business-Analytics/main/Consumer_Airfare_Report_Passenger_Table1_2019_2024q1.csv")
+
+#Filters dataset for the year 2024 quarter 1
+d_filter = d %>% filter(Year==2024 & quarter==1)
+
+#Takes first sample of 250 observations and places it into a csv
+d_sample = sample(d_filter, 250, replace = FALSE)
+write.csv(d_sample, "C:/BAN 542/sample1_CAR_2024q1")
+
+#Takes second sample of 250 observations and places it into another csv
+d_sample_2 = sample(d_filter, 250, replace = FALSE)
+write.csv(d_sample, "C:/BAN 542/sample2_CAR_2024q1")
+
+#Takes third sample of 250 observations and places it into another csv
+d_sample_3 = sample(d_filter, 250, replace = FALSE)
+write.csv(d_sample, "C:/BAN 542/sample3_CAR_2024q1")
